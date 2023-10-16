4fd1c54af6863caacad6b6344da470144045c9a0
Mesalm <ahmedmesalam96@gmail.com>
2023-10-16 12:18:18 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(News());
+}
+
+class News extends StatelessWidget {
+  const News({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
+
